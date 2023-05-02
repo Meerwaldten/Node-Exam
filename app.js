3af1b1a0bfbd66f34f61aae04f0eeee8a997bf36
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-05-02 09:51:01 +0200

    Forgot .env file!

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 import express from "express";
-
+import dotenv from "dotenv/config";
 const app = express();
 
 app.get("/", (req, res) => {
     res.send({ message: "Zando er en lort"});
 });
 
-
-
-
-
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 app.listen(PORT, () => console.log("Server is running on port: ", PORT));
