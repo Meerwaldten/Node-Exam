e6e830825bd28678ba8d55aad8f5f0f1041776f3
Meerwaldten <Mikkel_m_j@hotmail.com>
2023-05-05 13:54:21 +0200

    Added database connection and tried to add a few collections

--- app.js
+++ app.js
@@ -2,8 +2,15 @@ import express from "express";
 import dotenv from "dotenv/config";
 const app = express();
 
+app.use(express.json());
+
+import db from "./database/connection.js";
+db;
+
+
+
 app.get("/", (req, res) => {
-    res.send({ message: "Zando er en lort"});
+    res.send({ message: "Loui er en søøød"});
 });
 
 const PORT = process.env.PORT;
